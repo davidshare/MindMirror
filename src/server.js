ebd30e5fdeb1089887dc15bf101e60bbcf55a0f6
davidshare <davidessienshare@gmail.com>
2019-06-04 15:40:39 +0100

    chore(eslint): setup linting with eslint
    - install eslint packages as dev dependencies -
    	* "babel-eslint": "^10.0.1",
        * "eslint": "^5.16.0",
        * "eslint-config-airbnb": "^17.1.0",
        * "eslint-config-airbnb-base": "^13.1.0",
        * "eslint-plugin-import": "^2.17.3",
        * "eslint-plugin-jsx-a11y": "^6.2.1",
        * "eslint-plugin-react": "^7.13.0"
    - add .eslintrc file
    - add editor config file
    - add an eslintignore file
    
    [Finishes #166427272]

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 // server.js
 import express from 'express';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
   return res.status(200).send({'message': 'Welcome to MindMirror'});
-})
+});
 
-app.listen(3000)
+app.listen(3000);
+
+/* eslint-disable-next-line */
 console.log('app running on port ', 3000);
