f44914832b56473052f2bb22475f50190db9c866
davidshare <davidessienshare@gmail.com>
2019-06-05 10:30:16 +0100

    chore(database): setup database connection
    - install pg and dotenv
    - create database config file
    - create database connection file
    - write tests for the database connection
    
    [Finishes #166427274]

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 // server.js
 import express from 'express';
+import connection from './helpers/db_conn';
 
+const client = connection();
+client.connect();
+
+// client.connect();
 const app = express();
 
 app.use(express.json());
@@ -9,9 +14,15 @@ app.get('/', (req, res) => {
   return res.status(200).json('Welcome to MindMirror');
 });
 
-app.listen(3000);
+app.get('/now', async (req, res) => {
+  const query='SELECT NOW()';
+  const result = await client.query(query);
+  res.status(200).json({now: result.rows[0].now });
+});
+
+app.listen(3001);
 
 /* eslint-disable-next-line */
-console.log('app running on port ', 3000);
+console.log('app running on port ', 3001);
 
 export default app;
