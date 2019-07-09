1d69582b790c99063b817c9ebb3e455befcb72c7
davidshare <davidessienshare@gmail.com>
2019-07-09 23:10:45 +0100

    feat(signup): implement signup functionality
    - add controller file
    
    [Starts #167183944]

---
+++ src/routes/api/v1/userRoute.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import userController from '../../../controllers/userController';
+
+const router = Router();
+
+router.post('auth/signup', [], userController.signup);
+
+export default router;
